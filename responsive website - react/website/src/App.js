6c33d0e95d2f11bac0b5b7793f77098f98784e61
talita-aya <tatakuriki@gmail.com>
2021-09-15 14:54:44 -0300

    responsive navbar done

--- responsive website - react/website/src/App.js
+++ responsive website - react/website/src/App.js
@@ -1,8 +1,20 @@
+import React from "react";
+import NavBar from "./components/NavBar";
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+
+
 function App() {
   return (
-    <div className="App">
-      oi
-    </div>
+    <>
+      <Router>
+        <NavBar />
+
+        <Switch>
+          <Route path='/' exact />
+        </Switch>
+
+      </Router>
+    </>
   );
 }
 
